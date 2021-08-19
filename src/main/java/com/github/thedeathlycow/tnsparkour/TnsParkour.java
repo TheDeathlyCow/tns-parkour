6f044f600cbac4ce36488d2c9939b0e66d45c282
Michael Alpenfels <mwa172@uclive.ac.nz>
2021-08-19 18:20:06 +1200

    Initial commit

---
+++ src/main/java/com/github/thedeathlycow/tnsparkour/TnsParkour.java
@@ -0,0 +1,17 @@
+package com.github.thedeathlycow.tnsparkour;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TnsParkour extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
