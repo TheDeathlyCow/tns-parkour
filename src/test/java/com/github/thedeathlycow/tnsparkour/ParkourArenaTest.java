075dd4dbce11a113cb68c44a299eb904b0e05a9d
Michael Alpenfels <mwa172@uclive.ac.nz>
2021-09-10 17:58:14 +1200

    more test

--- src/test/java/com/github/thedeathlycow/tnsparkour/ParkourArenaTest.java
+++ src/test/java/com/github/thedeathlycow/tnsparkour/ParkourArenaTest.java
@@ -43,15 +43,61 @@ class ParkourArenaTest {
 
     @Test
     public void testRandomScores() {
-        int[] scoresArr = {997, 1002, 1003, 1035, 1045, 1051, 1091, 1099, 1297};
-        List<String> sortedScores = sortScores(scoresArr);
-        assertProperOrder(scoresArr, sortedScores);
+
+        LinkedHashMap<String, Integer> scores = new LinkedHashMap<>();
+        addScore(scores, "TheDeathlyCow", 997);
+        addScore(scores, "TheDeathlyCow", 1002);
+        addScore(scores, "TheDeathlyCow", 1003);
+        addScore(scores, "TheDeathlyCow", 1035);
+        addScore(scores, "TheDeathlyCow", 1045);
+        addScore(scores, "TheDeathlyCow", 1051);
+        addScore(scores, "TheDeathlyCow", 1091);
+        addScore(scores, "TheDeathlyCow", 1099);
+        addScore(scores, "TheDeathlyCow", 1297);
+
+        List<String> sortedScores = scores.keySet().stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(sortedScores);
+        assertProperOrder(scores, sortedScores);
+    }
+
+    @Test
+    public void testNamedScores() {
+
+        LinkedHashMap<String, Integer> scores = new LinkedHashMap<>();
+        addScore(scores, "Vaipereq", 17735);
+        addScore(scores, "Shalamander", 25642);
+
+        List<String> sortedScores = scores.keySet().stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(sortedScores);
+        assertProperOrder(scores, sortedScores);
+    }
+
+    private void assertProperOrder(LinkedHashMap<String, Integer> scores, List<String> sortedScores) {
+        List<Map.Entry<String, Integer>> scoresList = new ArrayList<>();
+        scoresList.addAll(scores.entrySet());
+
+        for (int i = 0; i < sortedScores.size(); i++) {
+            assertEquals(sortedScores.get(i), scoresList.get(i).getKey());
+        }
     }
 
     private void assertProperOrder(int[] scoresArr, List<String> sortedScores) {
         for (int i = 0; i < sortedScores.size(); i++) {
             assertTrue(sortedScores.get(i).endsWith(String.valueOf(scoresArr[i])));
         }
+    }
+
+    private void addScore(Map<String, Integer> scores, String name, int score) {
+        scores.put(getKey(name, score), score);
+    }
+
+    private String getKey(String name, int score) {
+        return String.format(ParkourArena.getSidebarOrder(score)
+                + "%s - %.3fs", name, score / 1000D);
     }
 
     public List<String> sortScores(int[] scores) {
