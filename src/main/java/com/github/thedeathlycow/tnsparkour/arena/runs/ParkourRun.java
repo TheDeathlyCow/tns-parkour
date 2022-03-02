4853022052aaaf43078a2693ff3fbf182ce58bef
Michael Alpenfels <mwa172@uclive.ac.nz>
2022-03-01 18:39:18 -1000

    hide particles on effects

--- src/main/java/com/github/thedeathlycow/tnsparkour/arena/runs/ParkourRun.java
+++ src/main/java/com/github/thedeathlycow/tnsparkour/arena/runs/ParkourRun.java
@@ -140,9 +140,9 @@ public class ParkourRun {
         removeRunnerEffects();
 
         List<PotionEffect> effects = Arrays.asList(
-                new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 999999, 255),
-                new PotionEffect(PotionEffectType.REGENERATION, 999999, 1),
-                new PotionEffect(PotionEffectType.SATURATION, 999999, 1)
+                new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 999999, 255, true, false),
+                new PotionEffect(PotionEffectType.REGENERATION, 999999, 1, true, false),
+                new PotionEffect(PotionEffectType.SATURATION, 999999, 1, true, false)
         );
         runner.addPotionEffects(effects);
 
